c825982ba71ceda9a31cea923d19103cf5c3e09f
Himanshu sharma <iamonlyhimanshusharma@gmail.com>
2021-09-07 10:58:58 +0530

    precaution and map added

--- assets/js/index.js
+++ assets/js/index.js
@@ -82,10 +82,35 @@ function show(data) {
       },
 
       animation: {
-        duration: 1000,
+        duration: 1500,
         easing: "linear",
         loop: false,
       },
     },
   });
+}
+
+
+//googl map api
+google.charts.load('current', {
+  'packages':['geochart'],
+});
+google.charts.setOnLoadCallback(drawRegionsMap);
+
+function drawRegionsMap() {
+  var data = google.visualization.arrayToDataTable([
+    ['Country', 'cases'],
+    ['Germany', 200],
+    ['United States', 300],
+    ['Brazil', 400],
+    ['Canada', 500],
+    ['France', 600],
+    ['RU', 700]
+  ]);
+
+  var options = {};
+
+  var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+  chart.draw(data, options);
 }
